ad1504314f3aedf6dd40cb2e752478c3bdbeac1c
Pavan Darshan <pavan.bhuvaneshwar@itorizon.com>
2024-12-17 18:22:44 +0530

    assignment

---
+++ javascript/front.js
@@ -0,0 +1,9 @@
+function openSideBar(){
+    document.getElementById("sideBar").style.display="block";
+    document.getElementById("menu_button").style.display='none';
+}
+
+function closeSideBar(){
+    document.getElementById("sideBar").style.display="none";
+    document.getElementById("menu_button").style.display='block';
+}
